9e35b7e01897fedb586b43b34bdbe86eb2f0cb5d
ASWIN-PALAYAD <aswinpalayad07@gmail.com>
2024-05-17 13:02:54 +0530

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'prettier/prettier': 0,
+    "quotes": ["error", "never"],
+    "no-trailing-spaces": ["error", { "skipBlankLines": true, "ignoreComments": true }],
+  },
 };
